f9527c7b7addb20ce5955e00152d41a9bebb759f
kelas177B <114228365+kelas177B@users.noreply.github.com>
2022-10-16 19:59:46 +0700

    Update sandimorse.py

--- sandimorse.py
+++ sandimorse.py
@@ -1,1 +1,15 @@
-print("hello, world")
+# '/' sebagai pemisah huruf abjad
+
+def to_morse_code(text):
+  code = {' ':'|', 'a':'/.-/', 'b':'/-.../', 'c':'/-.-./', 'd':'/-../',  'e':'/./', 'f':'/..-./', 'g':'/--./, 'h':'/..../', 'i':'/../', 'j':'/.---/', 'k':'/-.-/', 'l':'/.-../', 'm':'/--/', 'n':'/-./', 'o':'/---/', 'p':'/.--./', 'q':'/--.-/', 'r':'/.-./', 's':'/.../', 't':'/-/', 'u':'/..-/', 'v':'/...-/', 'w':'/.--/', 'x':'/-..-/', 'y':'/-.--/', 'z':'/--...'}
+          
+  morse_code = ""
+          
+  for x in text :
+      morse_code += code[x.lower()]
+  
+  return morse_code
+          
+text = input("Masukkan huruf: ")
+print(to_morse_code(text))
+          
